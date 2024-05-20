7454bd02258470cd90e8c09c46ccda8e8fa849b6
MAC <mac@MACs-MacBook-Pro.local>
2024-05-20 21:56:55 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: ['./src/**/*.{html,js'],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#4C5FD5',
+                secondary: '#dadbf1',
+            },
+            fontFamily: {
+                sans: ['GT America Trial', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+}
